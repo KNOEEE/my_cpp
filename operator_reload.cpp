ebab8089f84173f7c891364c630dc118c3489b1c
KNOEEE <1215892778@qq.com>
2021-08-21 13:27:44 +0800

    reload operator

--- operator_reload.cpp
+++ operator_reload.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+// TODO 
+// heuristic search, a* algorithm
 class Square
 {
 public:
@@ -13,7 +15,29 @@ public:
   int get_perimeter() {
     return a * b;
   }
+
+  // try reload operator (class member)
+  Square operator+(const Square& that) {
+    Square s;
+    // `this` can be omitted
+    s.a = a + that.a;
+    s.b = this->b + that.b;
+    return s;
+  }
+
+  Square operator-(int val) {
+    Square s;
+    s.a = a - val;
+    s.b = b - val;
+    return s;
+  }
 };
+
+Square operator^(const Square& aa, const Square& bb) {
+  int a = aa.a + bb.a;
+  int b = aa.b * bb.b;
+  return Square(a, b);
+}
 
 int main() {
   Square s1;
@@ -22,7 +46,17 @@ int main() {
   cout << s1.get_perimeter() << endl;
   Square* s2 = new Square(1, 4);
   cout << s2->get_perimeter() << endl;
-  shared_ptr<Square> s3;
+  shared_ptr<Square> s3, s4;
   s3.reset(new Square(4, 5));
   cout << s3->get_perimeter() << endl;
+  // notice arguments for make_shared
+  s4 = make_shared<Square>(2, 4);
+  cout << s4->get_perimeter() << endl;
+  Square s5 = s1 + *s4;
+  cout << s5.get_perimeter() << endl;
+  cout << (s5 - 2).get_perimeter() << endl;
+  auto s6 = s1 ^ (s5 - 4);
+  cout << s6.a << " " << s6.b <<
+      " " << s6.get_perimeter() << endl;
+  // how to set max column length
 }
